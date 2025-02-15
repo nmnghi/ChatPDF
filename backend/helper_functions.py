d5b7e8fc6cfe63e78ce4978fddf68896223ceb08
tamB2203579 <tamB2203579@student.ctu.edu.vn>
2025-02-15 21:46:17 +0700

    add stuff

---
+++ backend/helper_functions.py
@@ -0,0 +1,50 @@
+import os
+import fitz
+import pandas as pd
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+def read_pdf_to_string(filepath):
+    doc = fitz.open(filepath)
+    text = ""
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        text += page.get_text()
+    return text
+
+def chunking(text, embeddings):
+    text_splitter = SemanticChunker(
+        embeddings,
+        breakpoint_threshold_type="percentile",
+        breakpoint_threshold_amount= 90,
+        min_chunk_size=100
+    )
+    return text_splitter.create_documents([text])
+
+def encode_pdf(path):
+    text = read_pdf_to_string(path)
+    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    text_splitter = SemanticChunker(
+        embeddings,
+        breakpoint_threshold_type="percentile",
+        breakpoint_threshold_amount=90,
+        min_chunk_size=30
+    )
+    docs = text_splitter.create_documents([text])
+    vector_store = FAISS.from_documents(docs, embeddings)
+    return vector_store
+
+def save_csv(query, docs, path="training.csv"):
+    data= []
+    for doc, score in docs:
+        data.append({
+            "query": query,
+            "answer": doc.page_content,
+            "score": score
+        })
+    df = pd.DataFrame(data) 
+    if os.path.exists(path):
+        df.to_csv(path, mode='a', header=False, index=False, encoding='utf-8')
+    else:
+        df.to_csv(path, index=False, encoding='utf-8')
--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,15 @@
 import os
-import fitz
 from dotenv import load_dotenv 
 from fastapi import FastAPI, File, UploadFile, Form, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
-from langchain_experimental.text_splitter import SemanticChunker
+from helper_functions import *
 
 load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv('OPEN_API_KEY')
 
 app = FastAPI()
 
@@ -28,8 +27,6 @@ db_vectors = None
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
-OPEN_API_KEY = os.getenv("OPEN_API_KEY")
-
 @app.post("/response")
 def response(msg: str = Form(...)):
     return {"response": get_chat_response(msg)}
@@ -43,29 +40,9 @@ def upload_pdf(file: UploadFile = File(...)):
     with open(filepath, "wb") as buffer:
         buffer.write(file.file.read())
 
-    content = read_pdf_to_string(filepath)
-
-    embeddings = OpenAIEmbeddings(api_key=OPEN_API_KEY)
-    docs = chunking(content, embeddings)
-    db_vectors = FAISS.from_documents(docs, embeddings)
+    db_vectors = encode_pdf(filepath)
 
     return {"message": "File uploaded and processed successfully"}
-
-def read_pdf_to_string(filepath):
-    doc = fitz.open(filepath)
-    content = ""
-    for page in doc:
-        content += page.get_text()
-    return content
-
-def chunking(content, embeddings):
-    text_splitter = SemanticChunker(
-        embeddings,
-        breakpoint_threshold_type="percentile",
-        breakpoint_threshold_amount=90,
-        min_chunk_size=100
-    )
-    return text_splitter.create_documents([content])
 
 rag_template = """
 Use only the following context to answer the user's query in a well-formatted, concise, and clear manner paragraph. If you don't have an answer, respond "Tài liệu pdf mà bạn cung cấp không có thông tin cho câu hỏi của bạn!".
@@ -78,13 +55,15 @@ Trả lời:
 """
 
 rag_prompt = ChatPromptTemplate.from_template(rag_template)
-chat_model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, openai_api_key=OPEN_API_KEY)
+chat_model = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
 
 def get_chat_response(text: str):
     if db_vectors:
-        chunks_query_retriever = db_vectors.as_retriever(
-            search_kwargs={"k": 5, "score_threshold": 0.7}
-        )
+        chunks_query_retriever = db_vectors.as_retriever(search_kwargs={"k": 5, "score_threshold": 0.5})
+
+        docs = db_vectors.similarity_search_with_score(text, k=3)
+        save_csv(text, docs)
+
         semantic_rag_chain = (
             {"context": chunks_query_retriever, "question": RunnablePassthrough()}
             | rag_prompt
