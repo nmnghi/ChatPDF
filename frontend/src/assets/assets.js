a7d3eb0c60eab858511e6403e6f39c757c0f67bf
Nghi <nmnghi0109@gmail.com>
2025-03-13 20:33:14 +0700

    fix

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -4,9 +4,7 @@ import question_icon from './question_icon.png'
 import setting_icon from './setting_icon.png'
 import user_icon from './user_icon.png'
 import send_icon from './send_icon.png'
-import gemini_icon from './gemini_icon.png'
 import trash_icon from './trash_icon.png'
-import gemini_logo from './gemini-chatbot-logo.svg'
 
 export const assets = {
     history_icon,
@@ -15,7 +13,5 @@ export const assets = {
     setting_icon,
     user_icon,
     send_icon,
-    gemini_icon,
     trash_icon,
-    gemini_logo
 }
